dc08f45082ee79294ba52a0fa8cfea3d6ea8d642
Alex <16655043+Alex-Dash@users.noreply.github.com>
2024-06-18 09:04:57 -0400

    Added backend timestamp microservice

---
+++ backend_timestamp_microservice/files/README.md
@@ -0,0 +1,4 @@
+# Timestamp Microservice
+
+This is a completed project for a FreeCodeCamp Backend API certification.
+Do not use as is in a production environment.
---
+++ backend_timestamp_microservice/files/index.js
@@ -0,0 +1,57 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+ 
+app.get("/api/:date", function (req, res) {
+  try {
+    let dt_raw = req.params.date
+    let dt = new Date()
+    if(new RegExp(/^\d+$/).test("12323")){
+      dt = new Date(parseInt(dt_raw))
+    } else {
+      dt = new Date(dt_raw)
+    }
+    if(isNaN(dt.valueOf())){
+      res.json({error: "Invalid Date"})
+      return
+    }
+    res.json({
+      unix: dt.getTime(),
+      utc: dt.toGMTString()
+    })
+  } catch (error) {
+    res.json({error: error})
+  }
+});
+
+app.get("/api/", function (req, res) {
+  let dt = new Date()
+  res.json({
+    unix: dt.getTime(),
+    utc: dt.toGMTString()
+  })
+});
+
+
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
